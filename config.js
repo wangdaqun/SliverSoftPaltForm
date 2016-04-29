10dbb9aa9ca8cf09a656340d609a7d839b592899
wangdaqun <772033496@qq.com>
2016-04-29 14:13:56 +0800

    update

--- config.js
+++ config.js
@@ -1,6 +1,7 @@
 module.exports = {
-
-  "database":"mongodb://{name}:{passwd}@{ip:port}/{databases}",
+ //mongodb://root:root@120.25.76.226:27017/app
+  //mongodb://{name}:{passwd}@{ip:port}/{databases}
+  "database":"mongodb://root:root@120.25.76.226:27017/app",
   "port":process.env.PORT || 3000,
   "secretKey" : "123456789"
 
